c48f057d87571d3ba7c2eca595c47e49b0eb6d29
anton <anton.obukhov@vision.ee.ethz.ch>
2020-04-19 19:48:32 +0200

    add a hint for Q 1.2.b to make solving Q 1.4 easier

--- mtl/models/model_deeplab_v3_plus.py
+++ mtl/models/model_deeplab_v3_plus.py
@@ -28,6 +28,9 @@ class ModelDeepLabV3Plus(torch.nn.Module):
 
         features = self.encoder(x)
 
+        # Uncomment to see the scales of feature pyramid with their respective number of channels.
+        # print(", ".join([f"{k}:{v.shape[1]}" for k, v in features.items()]))
+
         lowest_scale = max(features.keys())
 
         features_lowest = features[lowest_scale]
